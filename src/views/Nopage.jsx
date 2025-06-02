75c82168bdd234b5feb394e09f0547add6233ebb
Cibles <122311693+c1bles@users.noreply.github.com>
2025-06-02 11:34:43 +0100

    Update 404 (Nopage.jsx)

--- src/views/Nopage.jsx
+++ src/views/Nopage.jsx
@@ -5,13 +5,13 @@ function Nopage() {
     <>
       {!localStorage.getItem("cloakFavicon") && (
         <Helmet>
-          <title>404 - Starlight</title>
+          <title>404 - Twilight</title>
         </Helmet>
       )}
       <div>
         <div className="flex flex-col justify-center items-center ">
           <h1 className="text-5xl font-bold">404</h1>
-          <p className="text-2xl">Page Not Found</p>
+          <p className="text-2xl">Awh page not found :≺</p>
           <a href="/" className="btn btn-primary mt-5">
             Go Home
           </a>
